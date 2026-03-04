86a4d5111f3550fd9a27070693917226d7ed1f40
Dhruv Brahmbhatt <dhruvbrahm007@gmail.com>
2026-03-04 15:35:50 +0530

    render build fixes

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
+echo "Installing yt-dlp..."
 pip install yt-dlp
 
-apt-get update
-apt-get install -y ffmpeg
+echo "Downloading static ffmpeg..."
+
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
+
+tar -xvf ffmpeg.tar.xz
+
+FF_DIR=$(find . -type d -name "ffmpeg-*amd64-static")
+
+cp $FF_DIR/ffmpeg /usr/local/bin/ffmpeg
+cp $FF_DIR/ffprobe /usr/local/bin/ffprobe
+
+chmod +x /usr/local/bin/ffmpeg
+chmod +x /usr/local/bin/ffprobe
+
+echo "FFmpeg installed successfully"
