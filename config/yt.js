4edecc8315df121d50941b3458af5bafb044cdba
Dhruv Brahmbhatt <dhruvbrahm007@gmail.com>
2026-03-04 15:44:26 +0530

    yt.js config fixes

--- config/yt.js
+++ config/yt.js
@@ -1,5 +1,3 @@
 import ytdlp from "yt-dlp-exec";
 
-export const yt = ytdlp.create(
-  "C:/Users/Dhruv/AppData/Local/Packages/pythonsoftwarefoundation.python.3.13_qbz5n2kfra8p0/localcache/local-packages/python313/Scripts/yt-dlp.exe"
-);
+export const yt = ytdlp;
